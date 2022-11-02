6f76b03d7d5cb6900898aa0edd0b05ae629eee3b
somacoder <gkbartleyjr@gmail.com>
2022-11-02 14:45:32 -0400

    Added bash run file

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python inference.py --img_path /mnt/d/Dropbox/Include/Ubuntu/keith.jpg --save_path /mnt/d/Dropbox/Include/Ubuntu/save.jpg --model_path ../Pytorch_Retinaface/retinaface.wts
